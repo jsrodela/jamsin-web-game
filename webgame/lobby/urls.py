cfafc42e8af66503d6940f3bf14557393174ddb3
egwkim <35051914+egwkim@users.noreply.github.com>
2022-07-21 13:30:02 +0900

    Add login system

--- webgame/lobby/urls.py
+++ webgame/lobby/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'lobby'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('signup', views.signup, name='signup'),
 ]
--- webgame/lobby/views.py
+++ webgame/lobby/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
 def index(request):
     return render(request, 'lobby/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user != None:
+            auth.login(request, user)
+            return redirect('lobby:index')
+        return redirect('lobby:login')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'lobby/login.html', {'form': form})
+    
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user)
+            return redirect('lobby:index')
+        return redirect('lobby:signup')
+    else:
+        form = UserCreationForm()
+        return render(request, 'lobby/signup.html', {'form': form})
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('lobby:index')
