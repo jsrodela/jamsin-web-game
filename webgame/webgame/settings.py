e1ce65988d018b4744b73f881787c11e91762b55
egwkim <35051914+egwkim@users.noreply.github.com>
2022-07-22 11:28:04 +0900

    Add base template

--- webgame/webgame/settings.py
+++ webgame/webgame/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'webgame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
