32aab2584f2dfc440a7331a5c0f6e7e5fa6d6ddc
egwkim <35051914+egwkim@users.noreply.github.com>
2022-08-10 22:01:38 +0900

    Add minesweeper model by 박성현

--- webgame/minesweeper/models.py
+++ webgame/minesweeper/models.py
@@ -1,3 +1,26 @@
+import uuid
+import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Minesweeper(models.Model):
+    creator = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='minesweeper_creator')
+
+    # Save content as json string
+    moves = models.TextField(null=True)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    created = models.DateTimeField(default=datetime.datetime.now)
+    completed = models.DateTimeField(null=True)
+
+    def create(user):
+        game = Minesweeper(creator=user)
+        game.save()
+        return game
+
+    def complete(self):
+        self.completed = datetime.datetime.now()
+        self.save()
