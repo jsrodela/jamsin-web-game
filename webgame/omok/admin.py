820e170452b594d24ab7b9eef7cc6909a98dc9a6
egwkim <35051914+egwkim@users.noreply.github.com>
2022-08-10 16:30:34 +0900

    Add omok model by 송현우, 이명훈

--- webgame/omok/admin.py
+++ webgame/omok/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Omok
+
+admin.site.register(Omok)
--- webgame/omok/models.py
+++ webgame/omok/models.py
@@ -1,3 +1,28 @@
+import uuid
+import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Omok(models.Model):
+    creator = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='omok_creator')
+    opponent = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='omok_opponent', null=True)
+
+    moves = models.TextField(null=True)
+    chat = models.TextField(null=True)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    created = models.DateTimeField(default=datetime.datetime.now)
+    completed = models.DateTimeField(null=True)
+
+    def create(user):
+        game = Omok(creator=user)
+        game.save()
+        return game
+
+    def complete(self):
+        self.completed = datetime.datetime.now()
+        self.save()
