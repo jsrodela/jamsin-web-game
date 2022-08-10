09964d3bad4c9ae9282a8a1ef378a64eda8a4004
egwkim <35051914+egwkim@users.noreply.github.com>
2022-08-10 23:29:14 +0900

    Add minesweeper consumer

--- webgame/minesweeper/consumers.py
+++ webgame/minesweeper/consumers.py
@@ -1,12 +1,39 @@
+import json
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
+
+from .models import Minesweeper
 
 
 class MinesweeperConsumer(WebsocketConsumer):
     def connect(self):
+        user = User.objects.get(username=self.scope['user'])
+        game = Minesweeper.create(user)
+        self.game = game
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        self.game.complete()
 
     def receive(self, text_data=None, bytes_data=None):
-        pass
+        text_data_json = json.loads(text_data)
+        if 'command' in text_data_json.keys():
+            command = text_data_json['command']
+            if command == 'set':
+                # TODO Create new board
+                size = text_data_json['size']
+                mine_cnt = text_data_json['num']
+                pass
+            if command == 'uncover':
+                # TODO Uncover selected cell and send result
+                x, y = text_data_json['pos']
+                pass
+            if command == 'flag':
+                # TODO Mark cell as flagged
+                x, y = text_data_json['pos']
+                pass
+            if command == 'unflag':
+                # TODO Unmark flagged cell
+                x, y = text_data_json['pos']
+                pass
--- webgame/minesweeper/static/minesweeper/script.js
+++ webgame/minesweeper/static/minesweeper/script.js
@@ -1,3 +1,7 @@
+const socket = new WebSocket(
+    "ws://" + window.location.host + "/ws/minesweeper/"
+);
+
 document.body.oncontextmenu = () => { return false; };
 document.body.onselectstart = () => { return false; };
 document.body.ondragstart = () => { return false; };
@@ -12,13 +16,20 @@ startBtn.addEventListener("click", set_game);
 function set_game() {
     size = parseInt(document.getElementById("size").value); //판 사이즈
     num = parseInt(document.getElementById("num").value); //지뢰 개수
+
+    socket.send(JSON.stringify({
+        command: 'set',
+        size: size,
+        num: num
+    }));
+
     set_table(size);
 
-    tdlist = document.getElementById("game_table").getElementsByTagName('td'); //테이블 td태그
+    tdlist = document.getElementById("game_table").getElementsByTagName("td"); //테이블 td태그
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            tdlist[size * i + j].addEventListener("click", left_click);
-            tdlist[size * i + j].addEventListener("contextmenu", right_click);
+            tdlist[size * i + j].addEventListener("click", left_click(i, j));
+            tdlist[size * i + j].addEventListener("contextmenu", right_click(i, j));
         }
     }
 }
@@ -36,14 +47,32 @@ function set_table(s) //int
     document.getElementById("area").innerHTML = tag;
 }
 
-function left_click() {
-    if (this.innerHTML != "🚩") {
-        this.className = "uncovered";
-    }
+function left_click(x, y) {
+    return function () {
+        if (this.className == "covered" && this.innerHTML != "🚩") {
+            send_command("uncover", x, y);
+            this.className = "uncovered";
+        }
+    };
 }
 
-function right_click() {
-    if (this.className == "covered") {
-        this.innerHTML = "🚩";
-    }
+function right_click(x, y) {
+    return function () {
+        if (this.className == "covered") {
+            if (this.innerHTML == "") {
+                send_command("flag", x, y);
+                this.innerHTML = "🚩";
+            } else {
+                send_command("unflag", x, y);
+                this.innerHTML = "";
+            }
+        }
+    };
+}
+
+function send_command(command, x, y) {
+    socket.send(JSON.stringify({
+        command: command,
+        pos: (x, y)
+    }));
 }
